f31262c8c5ade25dfa57da6c42189147134cbb32
vlad <iamvlad@gmail.com>
2016-06-29 15:58:01 -0700

    use $DATABASE_URL to initialize db

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-# to create tables
-psql -f observations.sql
-
 # this is for local development, since heroku will have this set automatically
 if [ "$DATABASE_URL" == "" ]
 then
   # for this environment variable to be set in the shell running this script, this script should be run with source, i.e. `source start.sh`
   export DATABASE_URL=postgres:///$(whoami)?sslmode=disable
 fi
+
+# to create tables
+psql $DATABASE_URL -f observations.sql
